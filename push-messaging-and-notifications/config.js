6d59ba91a98aa16e04410373e5d1aca93ad83f93
Gurprit Sahota <gurprit.sahota@secretsales.com>
2018-11-21 15:04:59 +0000

    updating api key

--- push-messaging-and-notifications/config.js
+++ push-messaging-and-notifications/config.js
@@ -1,4 +1,4 @@
 window.GoogleSamples = window.GoogleSamples || {};
 window.GoogleSamples.Config = window.GoogleSamples.Config || {
-  gcmAPIKey: '<Your Cloud Messaging API Key from https://console.firebase.google.com>'
+  gcmAPIKey: 'AAAA6B6nfwA:APA91bEYzDBlI08YWMfVSr-XPzi27RSJlGuFMwiGLKNrC-uB4C6mfyfSQrZthK6IO4RWbHCk9TlTkaPtixW3gU2FWRNALXQGIHVe2-ZRSt-8iuG2Ko0BJwk5R7RC2ko3OVZWC2aEJkmq'
 };
