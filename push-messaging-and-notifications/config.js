22649dd990611eac738f0b24961bcc9d3a56df42
Gurprit Sahota <gurprit.sahota@mous.co>
2022-02-18 16:35:11 +0000

    removing old files replace with game

--- push-messaging-and-notifications/config.js
+++
@@ -1,4 +0,0 @@
-window.GoogleSamples = window.GoogleSamples || {};
-window.GoogleSamples.Config = window.GoogleSamples.Config || {
-  gcmAPIKey: 'AAAA6B6nfwA:APA91bEYzDBlI08YWMfVSr-XPzi27RSJlGuFMwiGLKNrC-uB4C6mfyfSQrZthK6IO4RWbHCk9TlTkaPtixW3gU2FWRNALXQGIHVe2-ZRSt-8iuG2Ko0BJwk5R7RC2ko3OVZWC2aEJkmq'
-};
